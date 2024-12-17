c1f1528a5d4d7a05e05e36b0e4876f9016affe4f
Oleksii Synelnyk <alex0destroyer@gmail.com>
2024-12-17 22:48:47 +0200

    small fixes

--- src/main/java/com/leka/teashop/model/User.java
+++ src/main/java/com/leka/teashop/model/User.java
@@ -118,21 +118,6 @@ public class User implements UserDetails {
     }
 
     @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
     public boolean isEnabled() {
         return accountStatus == AccountStatus.ACTIVE;
     }
